084d14f6b96858f6e54ffda3d4c3af920ca3fa7a
Justin Canington <justacan@gmail.com>
2018-10-05 22:40:28 -0700

    mostly done with map generation

--- src/game/Dungeon.js
+++ src/game/Dungeon.js
@@ -1,11 +1,87 @@
 import AABB from "./AABB";
 
+import { astar, Graph } from './lib/astar';
+
+
+const makeGraph = (map, onlyFloors = false) => {
+  const grid = [];
+
+  const height = map.length
+  const width = map[0].length;
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (!grid[y]) grid[y] = [];
+      grid[y][x] = undefined;
+    }
+  }
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (!grid[y]) grid[y] = []
+      if (onlyFloors) {
+        grid[x][y] = (map[y][x].type === 'floor') ? 1 : 0;
+      } else {
+        grid[x][y] = map[y][x].weight;
+      }
+    }
+  }
+
+  return new Graph(grid);
+}
+
+
+const Floor = () => {
+  return {
+    type: 'floor',
+    weight: 1,
+    fillStyle: '#47361a'
+  }
+}
+
+const Wall = () => {
+  return {
+    weight: 5,
+    fillStyle: '#aab4b7'
+  }
+}
+
+const WallCorner = () => {
+  return {
+    weight: 0,
+    fillStyle: '#09b2e5'
+  }
+}
+
+const Void = () => {
+  return {
+    weight: 1,
+    fillStyle: '#000000'
+  }
+}
+
+const Yolo = () => {
+  return {
+    weight: 1,
+    fillStyle: '#17d81b'
+  }
+}
+
+const RoomCenter = () => {
+  return {
+    weight: 1,
+    fillStyle: '#eadc10'
+  }
+}
+
+
+
 function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 const addPadding = (room, padding = 1) => {
-  const {x, y, width, height} = room;
+  const { x, y, width, height } = room;
   return {
     x: x - padding,
     y: y - padding,
@@ -31,7 +107,7 @@ class Map {
   }
 
   iterate(func) {
-    const {width, height} = this;
+    const { width, height } = this;
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         func(x, y);
@@ -39,9 +115,10 @@ class Map {
     }
   }
 
-  findClosestRoom(room) {
+  findClosestRoom(room, filtered = false) {
     let closest = null;
-    for (let iRoom of this.rooms) {
+    const rooms = filtered || this.rooms
+    for (let iRoom of rooms) {
       // if (room.center.x === iRoom.center.x && room.center.y === iRoom.center.y) continue;
       if (room === iRoom) continue;
       if (iRoom.connected.includes(room)) continue;
@@ -53,12 +130,14 @@ class Map {
       const bx = Math.abs(room.center.x - closest.center.x);
       const by = Math.abs(room.center.y - closest.center.y);
 
-      if ( (ax+ay) < (bx+by) ) {
+      if ((ax + ay) < (bx + by)) {
         closest = iRoom;
       }
     }
     return closest;
   }
+
+  roomCounter = 0;
 
   placeRoom() {
     const x = getRandomArbitrary(0, this.width);
@@ -72,7 +151,7 @@ class Map {
 
     // Overlap check
     for (let room of this.rooms) {
-      if ( AABB(addPadding({x, y, width, height}), addPadding(room))) return false;
+      if (AABB(addPadding({ x, y, width, height }), addPadding(room))) return false;
     }
 
     const corners = [];
@@ -80,44 +159,28 @@ class Map {
     // Apply room to map
     for (let iY = y; iY < y + height; iY++) {
       for (let iX = x; iX < x + width; iX++) {
-        if ( ( iX === x && iY === y) || (iX === x && iY === y) ) corners.push(iX, iY)
         if (iY === y || iX === x || iY === y + height - 1 || iX === x + width - 1) {
-          this.setTile(iX, iY, 2);
+          this.setTile(iX, iY, Wall());
         } else {
-          this.setTile(iX, iY, 1);
+          this.setTile(iX, iY, Floor());
         }
+        if (iX === x && iY === y) this.setTile(iX, iY, WallCorner())
+        if (iX === x + width - 1 && iY === y) this.setTile(iX, iY, WallCorner())
+        if (iX === x && iY === y + height - 1) this.setTile(iX, iY, WallCorner())
+        if (iX === x + width - 1 && iY === y + height - 1) this.setTile(iX, iY, WallCorner())
 
       }
     }
 
     // Save room to be checked for overlap
-    this.rooms.push({x, y, width, height, connected: [], corners});
+    this.rooms.push({ index: this.roomCounter, x, y, width, height, connected: [], corners });
+    this.roomCounter++
     console.log("Room Placed!")
 
   }
 
   setTile(x, y, value) {
     this.map[y][x] = value;
-  }
-
-  findPathBetweenRooms(room1, room2) {
-    let pathCursor = {x: room1.center.x, y: room1.center.y};
-    while(true) {
-
-      if (pathCursor.x < room2.center.x) {
-        pathCursor.x++;
-      } else if (pathCursor.x > room2.center.x) {
-        pathCursor.x--;
-      } else if (pathCursor.y < room2.center.y) {
-        pathCursor.y++;
-      } else if (pathCursor.y > room2.center.y) {
-        pathCursor.y--;
-      }
-
-      this.setTile(pathCursor.x, pathCursor.y, 5)
-      if (pathCursor.x === room2.center.x && pathCursor.y === room2.center.y) return true;
-    }
-
   }
 
   connectRooms(room1, room2) {
@@ -127,61 +190,130 @@ class Map {
     room2.connected.push(room1);
   }
 
-  generate() {
-    this.iterate((x, y) => {
-      if (!this.map[y]) this.map[y] = []
-      this.setTile(x, y, 0);
-    });
-    console.log('Map Zeroed')
-
-    for (let i = 0;i < 1000; i++) {
-      this.placeRoom();
-    }
-
+  findRoomCenters() {
     for (let room of this.rooms) {
       let x = Math.floor(room.width / 2) + room.x;
       let y = Math.floor(room.height / 2) + room.y;
-      room.center = {x, y};
-      this.setTile(x, y, 4)
+      room.center = { x, y };
+      // this.setTile(x, y, RoomCenter())
+    }
+  }
+
+  generate() {
+    this.iterate((x, y) => {
+      if (!this.map[y]) this.map[y] = []
+      this.setTile(x, y, Void());
+    });
+
+    for (let i = 0; i < 1000; i++) {
+      this.placeRoom();
     }
 
+    this.findRoomCenters();
+
     for (let room of this.rooms) {
+      const graphWithWeight = makeGraph(this.map);
       const closest = this.findClosestRoom(room);
-      this.findPathBetweenRooms(room, closest);
+      const start = graphWithWeight.grid[room.center.x][room.center.y];
+      const end = graphWithWeight.grid[closest.center.x][closest.center.y];
+      const resultWithWeight = astar.search(graphWithWeight, start, end);
+      for (let res of resultWithWeight) {
+        this.setTile(res.x, res.y, Floor())
+      }
       this.connectRooms(room, closest);
     }
 
 
+    while (true) {
+      let areConnected = [];
+      let notConnected = [];
+      const firstRoom = this.rooms[0];
+      for (let room of this.rooms) {        
+        const graph = makeGraph(this.map, true);
+        const start = graph.grid[firstRoom.center.x][firstRoom.center.y];
+        const end = graph.grid[room.center.x][room.center.y];
+        const results = astar.search(graph, start, end);
+
+        if (results.length) areConnected.push(room)
+        // console.log(results.length);
+      }
+
+      areConnected.unshift(firstRoom)
+      notConnected = this.rooms.filter(e => !areConnected.includes(e))
+
+      console.log('areConnected:', areConnected.length);
+      console.log('notConnected:', notConnected.length);
+      console.log('rooms', this.rooms.length)
+
+      if (areConnected.length === this.rooms.length) {
+        console.log("ALL CONNECTED");
+        break;
+      }
+
+      
+
+      console.log('ISLANDS')
+      console.log("fixing island!")
+      console.log(notConnected)
+      // break;
+
+      try {
+        const firstUp = notConnected.pop();
+        console.log(">>", firstUp)
+        if (!firstUp) break;
+        const graphWithWeight = makeGraph(this.map);
+        const closest = this.findClosestRoom(firstUp, areConnected);
+        const start = graphWithWeight.grid[firstUp.center.x][firstUp.center.y];
+        const end = graphWithWeight.grid[closest.center.x][closest.center.y];
+        const resultWithWeight = astar.search(graphWithWeight, start, end);
+        for (let res of resultWithWeight) {
+          this.setTile(res.x, res.y, Floor())
+        }
+        this.connectRooms(firstUp, closest);
+      } catch (e) {
+        // this.render()
+        break;
+        console.log("BROKE")
+      }
 
 
 
-
-    // console.log(this.map);
+    }
   }
 
   render() {
-    const {width, height, tileSize} = this;
+    const { width, height, tileSize } = this;
+
+    // const graph = makeGraph(this.map, true);
+
+    // for (let y = 0; y < height; y++) {
+    //   for (let x = 0; x < width; x++) {
+    //     if (graph.grid[x][y].weight) {
+    //       this.ctx.fillStyle = "#ffffff"
+    //     } else {
+    //       this.ctx.fillStyle = "#000000"
+    //     }
+    //     this.ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+    //   }
+    // }
+
+    // for (let i in this.rooms) {
+    //   const room = this.rooms[i]
+    //   this.ctx.font = "12px Arial";
+    //   this.ctx.fillStyle = "#000000"
+    //   this.ctx.fillText(i, room.center.x * this.tileSize, room.center.y * this.tileSize);
+    // }
+
+    // return;
 
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
-        if (this.map[y][x] === 1 ) {
-          this.ctx.fillStyle="#7a4207";
-          this.ctx.fillRect(x * tileSize,y * tileSize,tileSize,tileSize);
+        if (!this.map[y][x]) {
+          this.ctx.fillStyle = "#ff00bf"
+        } else {
+          this.ctx.fillStyle = this.map[y][x].fillStyle
         }
-        if (this.map[y][x] === 2 ) {
-          this.ctx.fillStyle="#1449ce";
-          this.ctx.fillRect(x * tileSize,y * tileSize,tileSize,tileSize);
-        }
-        if (this.map[y][x] === 4 ) {
-          this.ctx.fillStyle="#f2da02";
-          this.ctx.fillRect(x * tileSize,y * tileSize,tileSize,tileSize);
-        }
-        if (this.map[y][x] === 5) {
-          this.ctx.fillStyle="#1d6d2a";
-          this.ctx.fillRect(x * tileSize,y * tileSize,tileSize,tileSize);
-        }
-
-
+        this.ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
       }
     }
   }
